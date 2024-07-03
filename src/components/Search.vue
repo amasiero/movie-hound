3f98b40ce6141288151b7291a8e900cec283ab50
Andrey Masiero <andreymasiero@gmail.com>
2024-07-04 00:20:37 +0100

    style(Search.vue): update Search component styles for better readability and consistency

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -5,11 +5,13 @@
 </script>
 
 <template>
-  <div class="bg-gray-300 w-64 md:w-72 flex items-center px-3 py-2 border border-gray-600 rounded-full">
-    <MagnifyingGlassIcon class="h-5 w-5 text-gray-800" />
+  <div
+    class="bg-gray-200 dark:bg-gray-700 w-64 md:w-72 flex items-center px-3 py-2 border border-gray-600 dark:border-gray-500 rounded-full"
+  >
+    <MagnifyingGlassIcon class="h-5 w-5 text-gray-800 dark:text-gray-300" />
     <input
       v-model="search"
-      class="px-2 border-none bg-gray-300 text-gray-800 outline-none"
+      class="px-2 border-none bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-300 outline-none placeholder-gray-400"
       type="text"
       placeholder="Search"
     />
